e650892e8ff11c365e4811bf38e86b923aeba053
Pranav Gurditta <pranavgurditta@gmail.com>
2020-11-06 14:38:10 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,23 @@
 from flask import Flask,render_template,request
-
+from flask import Flask, render_template, request
+from werkzeug import secure_filename
 import running_youtube
 app = Flask(__name__)
 
+
+
+
+@app.route('/upload')
+def upload_file():
+   return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def upload_file():
+   if request.method == 'POST':
+      f = request.files['file']
+      f.save(secure_filename(f.filename))
+      return 'file uploaded successfully'
+		
 @app.route("/")
 def home():
     return render_template("home.html")
