a0b39415bb9e93ff2f3df9706f32c5eb2f8e1d2b
Pranav Gurditta <pranavgurditta@gmail.com>
2020-11-07 15:26:19 +0530

    Update application.py

--- application.py
+++ application.py
@@ -6,18 +6,6 @@ app = Flask(__name__)
 
 
 
-
-@app.route('/upload')
-def upload_file():
-   return render_template('upload.html')
-	
-@app.route('/uploader', methods = ['GET', 'POST'])
-def upload_file():
-   if request.method == 'POST':
-      f = request.files['file']
-      f.save(secure_filename(f.filename))
-      return 'file uploaded successfully'
-		
 @app.route("/")
 def home():
     return render_template("home.html")
